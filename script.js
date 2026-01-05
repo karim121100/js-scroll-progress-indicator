5e6591bd9fff9f9b68b44ca6136bf0dd347deab7
KB <161012310+karim121100@users.noreply.github.com>
2026-01-05 23:42:47 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const scrollHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  const progress = (scrollTop / scrollHeight) * 100;
+  document.getElementById("progress").style.width = progress + "%";
+});
